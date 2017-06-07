cc7623a80780935d6c153a2f32ca780e5b0d9498
Amadeusz Winogrodzki <amadeusz.winogrodzki@gmail.com>
2017-06-07 22:57:32 +0200

    updated movie search component tests

--- src/components/MovieSearch/MovieSearch.test.jsx
+++ src/components/MovieSearch/MovieSearch.test.jsx
@@ -49,12 +49,80 @@ describe('MovieSearch', () => {
   it('should recognize a date on input change', () => {
     const wrapper = shallow(<MovieSearch />);
     const input = '2017-12-10';
-    wrapper.find(Select).props().onInputChange(input);
-    expect(
-      find(
-        wrapper.find(Select).props().optionGroups,
-        { label: 'movies.dates' }
-      ).options
-    ).toHaveLength(2);
+    wrapper.find(Select).simulate('inputChange', input);
+    const optionGroups = [];
+
+    expect(wrapper.find(Select).props().optionGroups).toContainEqual({
+      label: 'movies.dates',
+      filterByInput: false,
+      isUnique: true,
+      options: [
+        {
+          value: 'primary_release_date.lte',
+          label: `movies.date.primary_release_date.lte ${input}`,
+          type: 'date',
+          date: new Date(input),
+        },
+        {
+          value: 'primary_release_date.gte',
+          label: `movies.date.primary_release_date.gte ${input}`,
+          type: 'date',
+          date: new Date(input),
+        },
+      ],
+    });
+  });
+
+  it('should recognize a year on input change and pass according value to select', () => {
+    const wrapper = shallow(<MovieSearch />);
+    const input = '2017';
+    wrapper.find(Select).simulate('inputChange', input);
+    const optionGroups = [];
+
+    expect(wrapper.find(Select).props().optionGroups).toContainEqual({
+        label: 'movies.dates',
+        filterByInput: false,
+        isUnique: true,
+        options: [
+          {
+            value: 'primary_release_date.lte',
+            label: `movies.date.primary_release_date.lte ${input}`,
+            type: 'date',
+            date: new Date(input),
+          },
+          {
+            value: 'primary_release_year',
+            label: `movies.date.primary_release_year ${input}`,
+            type: 'date',
+            date: new Date(input).getFullYear(),
+          },
+          {
+            value: 'primary_release_date.gte',
+            label: `movies.date.primary_release_date.gte ${input}`,
+            type: 'date',
+            date: new Date(input),
+          },
+        ],
+    });
+  });
+
+  it('should get values on select change', () => {
+    const onChangeSpy = jest.fn();
+    const wrapper = shallow(<MovieSearch onChange={onChangeSpy} />);
+    const options = [
+      { label: 'Test', value: 'test', type: 'date' },
+      { label: 'Test1', value: 'test1', type: 'genre' },
+    ];
+    wrapper.find(Select).simulate('change', options);
+
+    expect(onChangeSpy).toBeCalledWith({
+      dates: [
+        { label: "Test", type: "date", value: "test" },
+      ],
+      genres: [
+        { label: "Test1", type: "genre", value: "test1" },
+      ],
+      sortBy: null,
+    });
   });
 });
