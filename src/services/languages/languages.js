c07c67d1a2e92462293965f554f2a13691ae002e
Amadeusz Winogrodzki <amadeusz.winogrodzki@gmail.com>
2017-06-12 20:30:26 +0200

    added languages service and test

---
+++ src/services/languages/languages.js
@@ -0,0 +1,37 @@
+import languages from 'languages';
+
+const languagesData = languages.getAllLanguageCode().map(
+  langCode => Object.assign({}, { code: langCode }, languages.getLanguageInfo(langCode))
+);
+
+class Languages {
+  getLike(value) {
+    if (typeof value !== 'string' || value.length < 1) {
+      return [];
+    }
+
+    return languagesData.filter((language) => {
+      if (
+        typeof language.name !== 'string'
+        || typeof language.nativeName !== 'string'
+      ) {
+        return false;
+      }
+
+      if (
+        this.isLike(value, language.name)
+        || this.isLike(value, language.nativeName)
+      ) {
+        return true;
+      }
+
+      return false;
+    });
+  }
+
+  isLike(value, fullString) {
+    return fullString.toLowerCase().indexOf(value.toLowerCase()) >= 0;
+  }
+}
+
+export default new Languages();
---
+++ src/services/languages/languages.test.js
@@ -0,0 +1,16 @@
+import languages from './languages';
+
+describe('languages service', () => {
+  describe('getLike', () => {
+    it('should be able to find data of a valid language', () => {
+      expect(languages.getLike('poli').length).toBeGreaterThan(0);
+      expect(languages.getLike('ENGLISH').length).toBeGreaterThan(0);
+      expect(languages.getLike('eng').length).toBeGreaterThan(0);
+      expect(languages.getLike('de').length).toBeGreaterThan(0);
+      expect(languages.getLike('').length).toBe(0);
+      expect(languages.getLike(null).length).toBe(0);
+      expect(languages.getLike('231').length).toBe(0);
+      expect(languages.getLike(123).length).toBe(0);
+    });
+  });
+});
