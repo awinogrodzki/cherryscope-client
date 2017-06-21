54f20fc771854a638018bd027f1cdf50611bcd48
Amadeusz Winogrodzki <amadeusz.winogrodzki@gmail.com>
2017-06-21 18:13:52 +0200

    removed headline

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import CherryscopeLogo from 'resources/images/cherryscope_logo.svg';
-import { t } from 'services/translate';
 import styles from './Header.css';
 
 const Header = () => (
@@ -10,7 +9,6 @@ const Header = () => (
       <Link to="/">
         <CherryscopeLogo className={styles.logo} />
       </Link>
-      <span className={styles.headline}>{t('header.headline')}</span>
     </div>
   </div>
 );
