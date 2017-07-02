bcebc1e37e9097a7fff2632e0e8abafe9b6840ff
Amadeusz Winogrodzki <amadeusz.winogrodzki@gmail.com>
2017-07-02 14:49:15 +0200

    feat(Movie): changed expansion logic and added expanded movie class to movie list

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -4,55 +4,74 @@ import classNames from 'classnames';
 import { t } from 'services/translate';
 import styles from './Movie.css';
 
-const renderImage = (url) => {
-  if (!url) {
+class Movie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.container = null;
+  }
+
+  setContainer(container) {
+    this.container = container;
+  }
+
+  onClick() {
+    this.container && this.props.onClick(this.container);
+  }
+
+  renderImage(url) {
+    if (!url) {
+      return (
+        <div
+          data-test="Movie.emptyImage"
+          className={styles.emptyImage}
+        />
+      );
+    }
+
     return (
-      <div
-        data-test="Movie.emptyImage"
-        className={styles.emptyImage}
-      />
+      <img data-test="Movie.image" src={url} />
     );
   }
 
-  return (
-    <img data-test="Movie.image" src={url} />
-  );
-};
-
-const Movie = ({
-  imageUrl,
-  title,
-  originalTitle,
-  voteAverage,
-  voteCount,
-  releaseDate,
-  className,
-}) => (
-  <div
-    className={classNames(
-    styles.container,
-    className
-  )}
-  >
-    <div className={styles.wrapper}>
-      <div className={styles.image}>
-        { renderImage(imageUrl) }
+  render() {
+    return (
+      <div
+        data-test="Movie.container"
+        ref={container => this.setContainer(container)}
+        className={classNames({
+          [styles.container]: true,
+          [styles.isExpanded]: !!this.props.isExpanded,
+          [this.props.className]: !!this.props.className,
+        })}
+      >
+        <div
+          data-test="Movie.wrapper"
+          className={styles.wrapper}
+          onClick={() => this.onClick()}
+          role="button"
+        >
+          <div className={styles.image}>
+            { this.renderImage(this.props.imageUrl) }
+          </div>
+          <div className={styles.content}>
+            <h4 className={styles.originalTitle}>{ this.props.originalTitle }</h4>
+            { this.props.originalTitle !== this.props.title
+                && <h6 className={styles.title}>{ this.props.title }</h6> }
+            <ul className={styles.data}>
+              <li className={styles.dataItem}>{`${t('movie.voteAverage')}:`} <strong>{this.props.voteAverage}</strong></li>
+              <li className={styles.dataItem}><strong>{this.props.voteCount}</strong> {t('movie.voteCount')}</li>
+              {
+                this.props.releaseDate !== null &&
+                <li className={styles.dataItem}>{t('movie.releaseDate')} <strong>{this.props.releaseDate.toLocaleDateString()}</strong></li>
+              }
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className={styles.content}>
-        <h4 className={styles.originalTitle}>{ originalTitle }</h4>
-        { originalTitle !== title && <h6 className={styles.title}>{ title }</h6> }
-        <ul className={styles.data}>
-          <li className={styles.dataItem}>{`${t('movie.voteAverage')}:`} <strong>{voteAverage}</strong></li>
-          <li className={styles.dataItem}><strong>{voteCount}</strong> {t('movie.voteCount')}</li>
-          {
-            releaseDate !== null &&
-            <li className={styles.dataItem}>{t('movie.releaseDate')} <strong>{releaseDate.toLocaleDateString()}</strong></li>
-          }
-        </ul>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 Movie.propTypes = {
   title: PropTypes.string,
@@ -62,6 +81,8 @@ Movie.propTypes = {
   voteCount: PropTypes.number,
   releaseDate: PropTypes.instanceOf(Date),
   className: PropTypes.string,
+  onClick: PropTypes.func,
+  isExpanded: PropTypes.bool,
 };
 
 Movie.defaultProps = {
@@ -72,6 +93,8 @@ Movie.defaultProps = {
   voteCount: null,
   releaseDate: null,
   className: null,
+  onClick: () => {},
+  isExpanded: false,
 };
 
 export default Movie;
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,42 +1,69 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import movieService from 'services/movie';
 import Movie from 'components/Movie';
 import styles from './MovieList.css';
 
-const renderLoading = (isLoading) => {
-  if (!isLoading) {
-    return null;
+class MovieList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expandedMovieId: null,
+    };
   }
 
-  return (
-    <div className={styles.loading}>
-      Loading...
-    </div>
-  );
-};
+  renderMovies() {
+    return this.props.movies.map(item => (
+      <Movie
+        className={classNames({
+          [styles.movie]: true,
+          [styles.isExpanded]: this.state.expandedMovieId === item.id,
+        })}
+        id={item.id}
+        key={item.id}
+        title={item.title}
+        originalTitle={item.original_title}
+        imageUrl={movieService.getImageUrl(item.poster_path)}
+        voteAverage={item.vote_average}
+        voteCount={item.vote_count}
+        releaseDate={new Date(item.release_date)}
+        onClick={() => {
+          if (this.state.expandedMovieId !== item.id) {
+            this.setState({ expandedMovieId: item.id });
+          } else {
+            this.setState({ expandedMovieId: null });
+          }
+        }}
+        isExpanded={this.state.expandedMovieId === item.id}
+      />
+    ));
+  }
 
-const renderMovies = movies => movies.map(item => (
-  <Movie
-    className={styles.movie}
-    key={item.id}
-    title={item.title}
-    originalTitle={item.original_title}
-    imageUrl={movieService.getImageUrl(item.poster_path)}
-    voteAverage={item.vote_average}
-    voteCount={item.vote_count}
-    releaseDate={new Date(item.release_date)}
-  />
-));
+  renderLoading() {
+    if (!this.props.isLoading) {
+      return null;
+    }
 
-const MovieList = ({ movies, isLoading }) => (
-  <div className={styles.container}>
-    { renderLoading(isLoading) }
-    <div className={styles.wrapper}>
-      { renderMovies(movies) }
-    </div>
-  </div>
-);
+    return (
+      <div className={styles.loading}>
+        Loading...
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        { this.renderLoading() }
+        <div className={styles.wrapper}>
+          { this.renderMovies() }
+        </div>
+      </div>
+    );
+  }
+}
 
 MovieList.propTypes = {
   movies: PropTypes.arrayOf(PropTypes.object),
