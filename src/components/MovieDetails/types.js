8ed74456eccb7d57a45823bf988ca7f01e1fcd3b
Amadeusz Winogrodzki <amadeusz.winogrodzki@gmail.com>
2017-07-08 14:29:46 +0200

    added types

---
+++ src/components/MovieDetails/types.js
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types';
+
+export const MovieDetailsPropTypes = {
+  originalTitle: PropTypes.string,
+  title: PropTypes.string,
+  imdbUrl: PropTypes.string,
+  overview: PropTypes.string,
+  isLoading: PropTypes.bool,
+  image: PropTypes.string,
+};
