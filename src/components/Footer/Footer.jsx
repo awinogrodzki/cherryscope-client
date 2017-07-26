a9165c4d1f1aeae7dc9f6474da77e0cd57b8d67b
Amadeusz Winogrodzki <amadeusz.winogrodzki@rst.com.pl>
2017-07-26 15:42:47 +0200

    feat(Footer): replaced text about the movie database with tmdb logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import { t } from 'services/translate';
+import TMDBLogo from './images/tmdb_logo.svg';
 import styles from './Footer.css';
 
 const Footer = () => (
   <div className={styles.container}>
-    <span className={styles.info}>
-      {t('footer.infoPrefix')}&nbsp;
-      <a rel="noopener noreferrer" target="_blank" href="https://www.themoviedb.org/">The Movie DB</a>.
-      &nbsp;{t('footer.infoSuffix')}
-    </span>
-    <br />
     <span className={styles.copyright}>
       © {(new Date()).getFullYear()}&nbsp;
       <a rel="noopener noreferrer" target="_blank" href="https://github.com/awinogrodzki">
         Amadeusz Winogrodzki
       </a>
     </span>
+    <a className={styles.tmdbLogo} rel="noopener noreferrer" target="_blank" href="https://www.themoviedb.org/">
+      <TMDBLogo />
+    </a>
   </div>
 );
 
