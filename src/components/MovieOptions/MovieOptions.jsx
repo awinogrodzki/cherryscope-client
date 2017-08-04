c2f5636af4b5f9c24ad385068f133e639260465e
Amadeusz Winogrodzki <amadeusz.winogrodzki@gmail.com>
2017-08-04 15:48:26 +0200

    feat(MovieOptions): added movie options component

--- src/components/MovieOptions/MovieOptions.jsx
+++ src/components/MovieOptions/MovieOptions.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import movieService from 'services/movie';
+import styles from './MovieOptions.css';
 import { movieType } from '../MovieList/types';
 
-const MovieOptions = () => (
-  <div>Movie Options</div>
+const renderImage = (url) => {
+  if (!url) {
+    return <div className={styles.emptyImage} />;
+  }
+
+  return (
+    <img src={url} />
+  );
+};
+
+const MovieOptions = ({
+  movies,
+  onMovieClick,
+}) => (
+  <button onClick={onMovieClick} className={styles.container}>
+    { movies.map(movie => (
+      <div className={styles.movie} key={movie.id}>
+        <div className={styles.image}>
+          {renderImage(movieService.getImageUrl(movie.poster_path, 160))}
+        </div>
+      </div>
+    )) }
+  </button>
 );
 
 MovieOptions.propTypes = {
