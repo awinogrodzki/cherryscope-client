88ec6fa394d43cde47a3fa76823bdfcd281fe47c
Amadeusz Winogrodzki <amadeusz.winogrodzki@gmail.com>
2017-08-04 14:34:43 +0200

    updated movie list tests

--- src/components/MovieList/MovieList.test.jsx
+++ src/components/MovieList/MovieList.test.jsx
@@ -3,6 +3,10 @@ import { shallow } from 'enzyme';
 import Movie from 'components/Movie';
 import MovieList from './MovieList';
 
+jest.mock('services/movie', () => ({
+  getImageUrl: url => url,
+}));
+
 describe('MovieList', () => {
   it('should display loading container if list is loading', () => {
     const wrapper = shallow(<MovieList isLoading />);
@@ -29,5 +33,29 @@ describe('MovieList', () => {
 
     wrapper.find(Movie).simulate('click', 'testElement');
     expect(onMovieSelectSpy).toHaveBeenCalledWith(12345, 'testElement');
+  });
+
+  it('should properly map movie type to movie component', () => {
+    const movies = [
+      {
+        id: 123,
+        title: 'title',
+        original_title: 'original_title',
+        poster_path: 'poster_path',
+        vote_average: 1,
+        vote_count: 2,
+        release_date: '2017-12',
+      },
+    ];
+    const wrapper = shallow(<MovieList movies={movies} isLoading={false} />);
+
+    expect(wrapper.find(Movie).props()).toMatchObject({
+      title: 'title',
+      originalTitle: 'original_title',
+      imageUrl: 'poster_path',
+      voteAverage: 1,
+      voteCount: 2,
+      releaseDate: new Date('2017-12'),
+    });
   });
 });
